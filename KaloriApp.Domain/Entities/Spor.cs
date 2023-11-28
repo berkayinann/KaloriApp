9a4e525e28ac391cbd9089e67ac41ef1fc9e7e15
gokcebulbul <gokcebulbul0@gmail.com>
2023-11-28 19:57:23 +0300

    Eklemeyapıldı

---
+++ KaloriApp.Domain/Entities/Spor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaloriApp.Domain.Entities
+{
+    public class Spor  : BaseEntity
+    {
+        public int SporID { get; set; }
+
+        public string SporName { get; set; }
+        
+        public string SporDescription { get; set; }
+
+        public double YakilanKalori { get; set; }
+
+    }
+}
