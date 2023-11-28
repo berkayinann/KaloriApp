db038c582e8e87fe1c244d2a22261f50b5fa8c54
Tarkovski7 <trkbgr1618@gmail.com>
2023-11-28 18:30:48 +0300

    tarifler eklendi

---
+++ KaloriApp.Domain/Entities/Recipes/Yemek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaloriApp.Domain.Entities.Recipes
+{
+    public class Yemek : BaseEntity
+    {
+        public string YemekAdi { get; set; }
+        public double KaloriMiktari { get; set; }
+        public string Kategori { get; set; }
+    }
+}
