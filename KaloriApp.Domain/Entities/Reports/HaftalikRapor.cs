2f1d5c19ff3f9f02f1bced3cf10d3a1024ce71ae
Tarkovski7 <trkbgr1618@gmail.com>
2023-11-28 17:02:49 +0300

    Raporları oluşturdum

---
+++ KaloriApp.Domain/Entities/Reports/HaftalikRapor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaloriApp.Domain.Entities.Reports
+{
+    public class HaftalikRapor
+    {
+        public DateTime HaftaBaslangic { get; set; }
+        public DateTime HaftaBitis { get; set; }
+        public List<Rapor> GunlukRaporlar { get; set; }
+    }
+}
