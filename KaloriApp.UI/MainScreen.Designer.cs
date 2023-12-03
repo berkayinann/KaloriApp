1d892bc95cf64bb74fd8dea354d7aac347a1554c
berkayinann <inan.berkay@outlook.com>
2023-12-03 09:24:32 +0300

    görsel

--- KaloriApp.UI/MainScreen.Designer.cs
+++ KaloriApp.UI/MainScreen.Designer.cs
@@ -28,12 +28,326 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainScreen";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            kryptonDateTimePicker1 = new Krypton.Toolkit.KryptonDateTimePicker();
+            groupBox2 = new GroupBox();
+            kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel2 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel3 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel4 = new Krypton.Toolkit.KryptonLabel();
+            kryptonPictureBox1 = new Krypton.Toolkit.KryptonPictureBox();
+            kryptonLabel5 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel6 = new Krypton.Toolkit.KryptonLabel();
+            kryptonTextBox1 = new Krypton.Toolkit.KryptonTextBox();
+            kryptonTextBox2 = new Krypton.Toolkit.KryptonTextBox();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = Properties.Resources.R;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Location = new Point(693, 342);
+            button1.Name = "button1";
+            button1.Size = new Size(46, 55);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImage = Properties.Resources.pngegg;
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            button2.FlatAppearance.BorderColor = Color.White;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Location = new Point(78, 346);
+            button2.Name = "button2";
+            button2.Size = new Size(118, 51);
+            button2.TabIndex = 1;
+            button2.Text = "Besin";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.BackgroundImage = Properties.Resources.pngegg;
+            button3.BackgroundImageLayout = ImageLayout.Stretch;
+            button3.FlatAppearance.BorderColor = Color.White;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = SystemColors.ButtonFace;
+            button3.Location = new Point(232, 346);
+            button3.Name = "button3";
+            button3.Size = new Size(118, 51);
+            button3.TabIndex = 2;
+            button3.Text = "Egzersiz";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Transparent;
+            button4.BackgroundImage = Properties.Resources.pngegg;
+            button4.BackgroundImageLayout = ImageLayout.Stretch;
+            button4.FlatAppearance.BorderColor = Color.White;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = SystemColors.ButtonFace;
+            button4.Location = new Point(385, 346);
+            button4.Name = "button4";
+            button4.Size = new Size(118, 51);
+            button4.TabIndex = 3;
+            button4.Text = "Raporlar";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Transparent;
+            button5.BackgroundImage = Properties.Resources.pngegg;
+            button5.BackgroundImageLayout = ImageLayout.Stretch;
+            button5.FlatAppearance.BorderColor = Color.White;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = SystemColors.ButtonFace;
+            button5.Location = new Point(539, 346);
+            button5.Name = "button5";
+            button5.Size = new Size(118, 51);
+            button5.TabIndex = 4;
+            button5.Text = "Tarifler";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.DodgerBlue;
+            groupBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(27, 21);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(199, 291);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Öneriler";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.DodgerBlue;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(6, 22);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(187, 262);
+            textBox1.TabIndex = 0;
+            // 
+            // kryptonDateTimePicker1
+            // 
+            kryptonDateTimePicker1.Location = new Point(539, 21);
+            kryptonDateTimePicker1.Name = "kryptonDateTimePicker1";
+            kryptonDateTimePicker1.Size = new Size(235, 21);
+            kryptonDateTimePicker1.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.DodgerBlue;
+            groupBox2.BackgroundImageLayout = ImageLayout.None;
+            groupBox2.Controls.Add(kryptonPictureBox1);
+            groupBox2.Controls.Add(kryptonLabel1);
+            groupBox2.Controls.Add(kryptonLabel2);
+            groupBox2.Controls.Add(kryptonLabel3);
+            groupBox2.Controls.Add(kryptonLabel4);
+            groupBox2.FlatStyle = FlatStyle.Flat;
+            groupBox2.Location = new Point(268, 21);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(199, 291);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Kişisel Bilgiler";
+            // 
+            // kryptonLabel1
+            // 
+            kryptonLabel1.Location = new Point(47, 167);
+            kryptonLabel1.Name = "kryptonLabel1";
+            kryptonLabel1.Size = new Size(62, 20);
+            kryptonLabel1.TabIndex = 8;
+            kryptonLabel1.Values.Text = "Ad Soyad";
+            // 
+            // kryptonLabel2
+            // 
+            kryptonLabel2.Location = new Point(47, 193);
+            kryptonLabel2.Name = "kryptonLabel2";
+            kryptonLabel2.Size = new Size(85, 20);
+            kryptonLabel2.TabIndex = 9;
+            kryptonLabel2.Values.Text = "Doğum Tarihi";
+            // 
+            // kryptonLabel3
+            // 
+            kryptonLabel3.Location = new Point(47, 219);
+            kryptonLabel3.Name = "kryptonLabel3";
+            kryptonLabel3.Size = new Size(45, 20);
+            kryptonLabel3.TabIndex = 10;
+            kryptonLabel3.Values.Text = "Ağırlık";
+            // 
+            // kryptonLabel4
+            // 
+            kryptonLabel4.Location = new Point(47, 245);
+            kryptonLabel4.Name = "kryptonLabel4";
+            kryptonLabel4.Size = new Size(31, 20);
+            kryptonLabel4.TabIndex = 11;
+            kryptonLabel4.Values.Text = "Boy";
+            // 
+            // kryptonPictureBox1
+            // 
+            kryptonPictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            kryptonPictureBox1.Location = new Point(47, 22);
+            kryptonPictureBox1.Name = "kryptonPictureBox1";
+            kryptonPictureBox1.Size = new Size(110, 122);
+            kryptonPictureBox1.TabIndex = 12;
+            kryptonPictureBox1.TabStop = false;
+            // 
+            // kryptonLabel5
+            // 
+            kryptonLabel5.Location = new Point(576, 81);
+            kryptonLabel5.Name = "kryptonLabel5";
+            kryptonLabel5.Size = new Size(126, 20);
+            kryptonLabel5.TabIndex = 13;
+            kryptonLabel5.Values.Text = "Ne kadar ml Su içtin?";
+            // 
+            // kryptonLabel6
+            // 
+            kryptonLabel6.Location = new Point(576, 139);
+            kryptonLabel6.Name = "kryptonLabel6";
+            kryptonLabel6.Size = new Size(151, 20);
+            kryptonLabel6.TabIndex = 14;
+            kryptonLabel6.Values.Text = "Ne kadar Dakika Uyudun?";
+            // 
+            // kryptonTextBox1
+            // 
+            kryptonTextBox1.InputControlStyle = Krypton.Toolkit.InputControlStyle.PanelClient;
+            kryptonTextBox1.Location = new Point(576, 107);
+            kryptonTextBox1.Name = "kryptonTextBox1";
+            kryptonTextBox1.PaletteMode = Krypton.Toolkit.PaletteMode.Office2007BlueDarkMode;
+            kryptonTextBox1.Size = new Size(85, 20);
+            kryptonTextBox1.TabIndex = 15;
+            // 
+            // kryptonTextBox2
+            // 
+            kryptonTextBox2.InputControlStyle = Krypton.Toolkit.InputControlStyle.PanelClient;
+            kryptonTextBox2.Location = new Point(576, 165);
+            kryptonTextBox2.Name = "kryptonTextBox2";
+            kryptonTextBox2.PaletteMode = Krypton.Toolkit.PaletteMode.Office2007BlueDarkMode;
+            kryptonTextBox2.Size = new Size(85, 20);
+            kryptonTextBox2.TabIndex = 16;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Transparent;
+            button6.BackgroundImage = (Image)resources.GetObject("button6.BackgroundImage");
+            button6.BackgroundImageLayout = ImageLayout.Stretch;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(674, 107);
+            button6.Name = "button6";
+            button6.Size = new Size(28, 26);
+            button6.TabIndex = 17;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Transparent;
+            button7.BackgroundImage = (Image)resources.GetObject("button7.BackgroundImage");
+            button7.BackgroundImageLayout = ImageLayout.Stretch;
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(674, 165);
+            button7.Name = "button7";
+            button7.Size = new Size(28, 26);
+            button7.TabIndex = 18;
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Transparent;
+            button8.BackgroundImage = Properties.Resources.pngegg;
+            button8.BackgroundImageLayout = ImageLayout.Stretch;
+            button8.FlatAppearance.BorderColor = Color.White;
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.ForeColor = SystemColors.ButtonFace;
+            button8.Location = new Point(576, 214);
+            button8.Name = "button8";
+            button8.Size = new Size(118, 51);
+            button8.TabIndex = 19;
+            button8.Text = "Profilini Güncele";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // MainScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Blue_Minimalist_Monochrome_Simple_Presentation_Template;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(kryptonTextBox2);
+            Controls.Add(kryptonTextBox1);
+            Controls.Add(kryptonLabel6);
+            Controls.Add(kryptonLabel5);
+            Controls.Add(groupBox2);
+            Controls.Add(kryptonDateTimePicker1);
+            Controls.Add(groupBox1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            Name = "MainScreen";
+            Text = "MainScreen";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private Krypton.Toolkit.KryptonDateTimePicker kryptonDateTimePicker1;
+        private GroupBox groupBox2;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel3;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel4;
+        private Krypton.Toolkit.KryptonPictureBox kryptonPictureBox1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel5;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel6;
+        private Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private Krypton.Toolkit.KryptonTextBox kryptonTextBox2;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
